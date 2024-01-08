108aeea4999eea4eb302631bce93bd0966399884
Oleg Kim <oleg.k@epcvip.com>
2024-01-09 03:18:12 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const URL = "https://open.er-api.com/v6/latest/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [baseCurrency, setBaseCurrency] = React.useState("USD");
+    const [baseValue, setBaseValue] = React.useState(1);
+    const [currencies, setCurrencies] = React.useState([baseCurrency]);
+
+    const [rates, setRates] = React.useState();
+    const [updated, setUpdated] = React.useState();
+
+    React.useEffect(() => {
+        fetch(URL + baseCurrency)
+            .then((res) => res.json())
+            .then((data) => {
+                setRates(data.rates);
+                setUpdated(new Date());
+            });
+    }, []);
+
+    function processCurrency(currency) {
+        if (!rates) {
+            return -1;
+        }
+
+        const rate = rates[currency] / rates[baseCurrency];
+        const value = baseValue * rate;
+        return value;
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="text-sm font-light">
+                    Last updated: {updated ? updated.toISOString() : "Never"}
+                </div>
+                <div className="mx-auto w-full max-w-sm">
+                    <div
+                        id="latest_rates_display"
+                        className="mx-auto my-5 w-full max-w-sm bg-white shadow rounded-md px-5 py-3 text-sm empty:hidden divide-y divide-dotted divide-slate-300 text-black"
+                    >
+                        {currencies.map((currency, index) => (
+                            <div
+                                className="flex items-center justify-between py-2"
+                                key={index}
+                            >
+                                <strong
+                                    className="cursor-pointer"
+                                    onClick={() => {
+                                        setCurrencies(
+                                            currencies.filter(
+                                                (_, ix) => ix !== index
+                                            )
+                                        );
+                                    }}
+                                >
+                                    {currency}
+                                </strong>
+                                <span
+                                    className="cursor-pointer"
+                                    onClick={(e) => {
+                                        const value = prompt("Enter new value");
+                                        if (value) {
+                                            setBaseCurrency(currency);
+                                            setBaseValue(parseFloat(value));
+                                        }
+                                    }}
+                                >
+                                    {processCurrency(currency)}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                    <div
+                        className="cursor-pointer"
+                        onClick={() => {
+                            const currency = prompt("Enter currency code");
+                            if (currency) {
+                                setCurrencies([...currencies, currency]);
+                            }
+                        }}
+                    >
+                        +
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
